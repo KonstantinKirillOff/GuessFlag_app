05423d0864c5341435bdfb6451a7cdd5be0d6a00
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2022-01-04 14:40:14 +0300

    Initial Commit

---
+++ GuessFlag_app/GuessFlag_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlag_appApp.swift
+//  GuessFlag_app
+//
+//  Created by Константин Кириллов on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlag_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
